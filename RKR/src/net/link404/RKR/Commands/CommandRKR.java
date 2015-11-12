2ed2ce74443cacfc9300a4822f93439f147c5a40
nbrandwine <nbrandwine@gmail.com>
2015-11-11 20:02:14 -0800

    Blah

--- RKR/src/net/link404/RKR/Commands/CommandRKR.java
+++ RKR/src/net/link404/RKR/Commands/CommandRKR.java
@@ -16,40 +16,43 @@ public class CommandRKR implements CommandExecutor
 						
 						if (commandLabel.equalsIgnoreCase("rkr")) // Base Command
 						{
-							if(args[0] == "hello")
+							if(args.length >= 1)
 							{
-								if(p.hasPermission("rkr.rename-me") || p.isOp())
+								if(args[0] == "hello")
 								{
-									// TODO:
-									// Fix this command to be something useful.
-									p.sendMessage(ChatColor.GREEN + "Hello, " + p.getName() + "!");
+									if(p.hasPermission("rkr.rename-me") || p.isOp())
+									{
+										// TODO:
+										// Fix this command to be something useful.
+										p.sendMessage(ChatColor.GREEN + "Hello, " + p.getName() + "!");
+									}
+								return true; 
 								}
-							}
-							else if(args[0] == "set")
-							{
-								if(p.hasPermission("rkr.set") || p.isOp())
+								else if(args[0] == "set")
 								{
-									// TODO:
-									// Communicate with PlatformConfigBuilder to determine <SQL/FF/YML>
-									// Collect command values from above, then talk to Platform<SQL/FF/YML>
-									// Store values and alert user of such actions.
-									p.sendMessage(ChatColor.GREEN + "Hello, " + p.getName() + "! You have reached 'rkr set'.");
+									if(p.hasPermission("rkr.set") || p.isOp())
+									{
+										// TODO:
+										// Communicate with PlatformConfigBuilder to determine <SQL/FF/YML>
+										// Collect command values from above, then talk to Platform<SQL/FF/YML>
+										// Store values and alert user of such actions.
+										p.sendMessage(ChatColor.GREEN + "Hello, " + p.getName() + "! You have reached 'rkr set'.");
+									}
+								return true;
 								}
-							}
-							else
-							{
-									// The command that basically says "You fucked up with your commands.".
-								p.sendMessage(ChatColor.DARK_AQUA + "-=====<" + ChatColor.AQUA + "Rival Kingdoms Reloaded" + ChatColor.DARK_AQUA + " >=====-");
-								p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "Rival Kingdoms Reloaded");
-								p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "Version 1.0 (DEV)");
-								p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "Rival Kingdoms Reloaded is a plugin designed to provide infrastructure for the server.");
-								p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "Developers");
-								p.sendMessage(ChatColor.AQUA + "- " + ChatColor.DARK_AQUA + "Stryker_");
-								p.sendMessage(ChatColor.AQUA + "- " + ChatColor.DARK_AQUA + "EUFlux");
-								p.sendMessage(ChatColor.AQUA + "- " + ChatColor.DARK_AQUA + "Zxios");
-								p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "This plugin is for administrators only.");
-							}
-							
+								else
+								{
+										// The command that basically says "You fucked up with your commands.".
+									p.sendMessage(ChatColor.DARK_AQUA + "-=====<" + ChatColor.AQUA + "Rival Kingdoms Reloaded" + ChatColor.DARK_AQUA + " >=====-");
+									p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "Rival Kingdoms Reloaded");
+									p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "Version 1.0 (DEV)");
+									p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "Rival Kingdoms Reloaded is a plugin designed to provide infrastructure for the server.");
+									p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "Developers");
+									p.sendMessage(ChatColor.AQUA + "- " + ChatColor.DARK_AQUA + "Stryker_");
+									p.sendMessage(ChatColor.AQUA + "- " + ChatColor.DARK_AQUA + "EUFlux");
+									p.sendMessage(ChatColor.AQUA + "- " + ChatColor.DARK_AQUA + "Zxios");
+									p.sendMessage(ChatColor.AQUA + "" + ChatColor.DARK_AQUA + "This plugin is for administrators only.");
+								return true; } }
 						}
 					}
 
